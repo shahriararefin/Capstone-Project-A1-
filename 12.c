4d0d0474abe6290c9cd7fb9dc12e1548310b6c14
shahriararefin <shahriararefin7@gmail.com>
2022-08-12 21:04:51 +0600

    Final

---
+++ 12.c
@@ -0,0 +1,16 @@
+// Id:221-35-1018
+
+#include<stdio.h>
+
+int main() {
+
+    int odd, even, result, num;
+
+    printf("Enter a value \n");
+    scanf("%d", &num);
+
+    result= (num%2)? printf("The number %d is odd ",num):printf("The number %d is even ", num);
+
+
+    return 0;
+}
