eacaa0b587d62b5914fa5d03af110c943b3b48f9
shahriararefin <shahriararefin7@gmail.com>
2022-08-12 20:56:41 +0600

    First Push

---
+++ 16.c
@@ -0,0 +1,23 @@
+// Id:221-35-1018
+
+#include<stdio.h>
+int main(){
+
+    int a,b,c, angle;
+
+    printf("Input three angle's value of a triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    angle= a+b+c;
+
+    if(angle==180)
+    {
+        printf("Triangle is Valid");
+    }
+    else
+    {
+        printf("Triangle is not valid");
+    }
+
+    return 0;
+}
