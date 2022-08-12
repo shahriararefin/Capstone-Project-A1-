5b9289166868e062aecb54e6cacb4c3f113d88c1
shahriararefin <shahriararefin7@gmail.com>
2022-08-12 20:26:00 +0600

    First Push

---
+++ 7.c
@@ -0,0 +1,21 @@
+// Id:221-35-1018
+
+#include<stdio.h>
+int main(){
+
+    int num, remainder, sum=0;
+    printf("Enter five digit number: ");
+    scanf("%d", &num);
+
+    while (num!=0)
+    {
+        remainder=num%10;
+        sum+=remainder;
+        num/=10;
+    }
+
+    printf("Sum of the number is : %d", sum);
+
+    return 0;
+    
+}
