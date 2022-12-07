ff36c7081aae7156bd8850810d46a61bb3bcf01b
Shahriar Arefin <65588285+shahriararefin@users.noreply.github.com>
2022-12-07 16:14:19 +0600

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,25 +1,121 @@
-// Id:221-35-1018
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include<stdio.h>
+void option();
+int fact(int n) {
+    if(n == 0) {
+        return 1;
+    }
+
+    return (n * fact(n - 1));
+}
 
 int main() {
+    int opt, choice = 1;
 
-    float gross_salary, dearness_allowance, basic_salary, house_rent;
-    printf("Enter Niloys Basic Salary\n");
-    scanf("%f", &basic_salary);
+    while(choice) {
+        option();
+        printf("Choice an option: ");
+        scanf("%d", &opt);
 
-    dearness_allowance= basic_salary-40%
-    printf("Enter dearness allowance: \n");
-    scanf("%f", &dearness_allowance);
+        switch(opt) {
+            case 1: {
+                    int *arr;
+                    int n, i, max;
 
-    house_rent= basic_salary-20%
-    printf("Enter the house rent \n");
-    scanf("%f", &house_rent);
+                    printf("Enter the size of the array: ");
+                    scanf("%d", &n);
 
-    gross_salary=basic_salary+dearness_allowance+house_rent;
+                    arr = (int *)malloc(sizeof(int) * n);
 
-    printf("Niloys gross salary is %.0f", gross_salary);
+                    printf("Enter %d element for element's of array: ", n);
+                    for(i = 0; i < n; i++) {
+                        scanf("%d",(arr+i));
+                    }
 
+                    max = *(arr);
 
+                    for(i = 0; i < n; i++) {
+                        if(*(arr+i) > max) {
+                            max = *(arr+i);
+                    }
+                }
+
+                printf("The maximum value of the array is %d\n", max);
+            }
+            break;
+
+            case 2:
+                {
+                    int num;
+                    printf("Enter a number for test: ");
+                    scanf("%d", &num);
+
+                    if(num == 0) {
+                        printf("%d is zero\n", num);
+                    }
+                    else if(num % 2 == 0) {
+                        printf("%d is even number\n", num);
+                    }
+                    else {
+                        printf("%d is odd number\n", num);
+                    }
+                }
+            break;
+
+            case 3:
+                {
+                    int len, i, j, com;
+                    char word[20], reverse_word[20];
+
+                    printf("Write a word for check: ");
+                    scanf("%s", word);
+
+                    len = strlen(word);
+
+                    for(i = 0, j = (len - 1); i < len; i++, j--) {
+                        reverse_word[j] = word[i];
+                    }
+
+                    com = strcmp(word, reverse_word);
+            
+                    if(com == 0) {
+                        printf("%s is palindrome\n", word);
+                    }
+                    else {
+                        printf("%s is not palindrome\n", word);
+                    }
+                }
+                break;
+
+            case 4:
+                {
+                    int num, result;
+                    printf("Enter a number for find it's factorial: ");
+                    scanf("%d", &num);
+                    
+                    result = fact(num);
+
+                    printf("Factorial of %d is %d\n", num, result);
+                }
+            break;
+            
+
+            default:
+                printf("Invalid input______");
+            break;
+        }
+        printf("Do you want to continue?\n(press zero(0) for exit): ");
+        scanf("%d", &choice);
+        
+    }
     return 0;
+}
+
+void option() {
+    printf("1. For find maximum in an array\n");
+    printf("2. Check odd even numbers\n");
+    printf("3. Check palindrome\n");
+    printf("Factorial with recursion\n");
 }
